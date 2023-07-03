6c655d56717228b7caf1bbb419421b7b24c18c16
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-07-03 12:42:43 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoryListArr = document.querySelectorAll('.item');
+console.log(`Number of categories: ${document.querySelectorAll('.item').length}`);
+
+categoryListArr.forEach(function (elem) {
+    console.log(`Category:${elem.firstElementChild.textContent}`);
+    console.log(`Elements:${elem.querySelectorAll('li').length}`);
+});

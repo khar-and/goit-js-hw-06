ab919f477b7be532d46b26a5a5364a8f3f09a5c9
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-07-03 14:31:28 +0300

    2

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const categoryListArr = document.querySelectorAll('.item');
 console.log(`Number of categories: ${document.querySelectorAll('.item').length}`);
 
-categoryListArr.forEach(function (elem) {
+categoryListArr.forEach((elem)=> {
     console.log(`Category:${elem.firstElementChild.textContent}`);
     console.log(`Elements:${elem.querySelectorAll('li').length}`);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemsIngredients = [];
+for (let i = 0; i < ingredients.length; i++) {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredients[i];
+  itemEl.classList.add('item');
+  itemsIngredients.push(itemEl);
+}
+const ingredientsListEL = document.querySelector('ul');
+ingredientsListEL.append(...itemsIngredients);
+

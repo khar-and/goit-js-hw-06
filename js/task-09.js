53ddf035b6c3b8efe53338cd488d142d0122e9f3
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-07-04 23:25:21 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+  
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+
+buttonEl.addEventListener('click', onChangeBodyColor);
+
+function onChangeBodyColor(event) {
+  let randomColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+}

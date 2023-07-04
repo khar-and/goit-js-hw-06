7eec596cf7ee26aeff35b4abb38c12197b1f3f3d
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-07-04 15:29:57 +0300

    6

---
+++ js/task-06.js
@@ -0,0 +1,19 @@
+const textInput = document.querySelector('input');
+const inputLength = textInput.getAttribute('data-length');
+
+textInput.addEventListener('focus', onReturnFocusInput);
+textInput.addEventListener('blur', onOffFocus);
+
+function onReturnFocusInput(event) {
+    textInput.classList.remove('invalid');
+}
+
+function onOffFocus(event) {
+    console.log('off');
+    if (Number(inputLength) !== Number(textInput.value.length)) {
+        textInput.classList.add('invalid');
+        console.log('не равно 6');
+    }
+    textInput.classList.add('valid');
+    console.log(textInput.value.length);
+}

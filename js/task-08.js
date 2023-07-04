08558ecc3f6a399edc3dd7324ecb3da922e83d10
Andrii Kharchenko <kh.andrew16@gmail.com>
2023-07-04 22:53:53 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit',onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    if (email === "" || password === "") {
+        alert('всі поля повинні бути заповнені');
+    } else{
+        const formData = { email, password };
+        console.log(formData);
+        formEl.reset();
+    }
+    
+}
